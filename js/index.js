ad8d29bbb95084b7e8092f09db8df46567f4d79d
prophetam119 <110048605+prophetam119@users.noreply.github.com>
2022-09-12 20:53:23 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,2 +1,26 @@
-"use strict"
-console.log('test')
+"use strict";
+
+function showPopup() {
+    document.getElementById("popup").style.display = "block";
+    document.getElementById("popup").style.animation = "show .2s ease";
+    document.getElementById("main").style.display = "none";
+  }
+  
+  function hidePopup() {
+    document.getElementById("popup").style.animation = "hide .2s ease";
+    setTimeout(function () {
+      document.getElementById("popup").style.display = "none";
+      document.getElementById("main").style.display = "block";
+    }, 150);
+  }
+
+  const element = document.querySelector("#scroll__container");
+
+  element.addEventListener('wheel', (event) => {
+    event.preventDefault();
+  
+    element.scrollBy({
+      left: event.deltaY < 0 ? -100 : 100,
+      
+    });
+  });
